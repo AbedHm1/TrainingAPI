dde6ca1486d2569c64c5b1d818d0906ab37ffac9
Abed Hm <kitkat.abedhammoud@gmail.com>
2024-08-08 07:35:50 +0300

    C1

---
+++ app.js
@@ -0,0 +1,32 @@
+/*
+const express = require('express')
+const app = express()
+
+require('dotenv').config()
+
+
+const PORT = process.env.PORT || 3030;
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+app.listen(PORT, () => {
+    console.log(`server started on port ${PORT}`)
+    
+})
+*/
+
+
+const express = require('express')
+const app = express()
+
+const PORT = process.env.PORT || 3030;
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+app.listen(PORT, () => {
+    console.log(`server started on port ${PORT}`)
+})
